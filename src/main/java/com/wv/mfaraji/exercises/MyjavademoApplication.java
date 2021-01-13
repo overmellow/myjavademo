088d5abbae3c1b86ca4f87076cdb71534802aa8f
overmellow <morteza_faraji@yahoo.com>
2021-01-12 18:11:37 -0800

    first commit

---
+++ src/main/java/com/wv/mfaraji/exercises/MyjavademoApplication.java
@@ -0,0 +1,13 @@
+package com.wv.mfaraji.exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjavademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyjavademoApplication.class, args);
+	}
+
+}
