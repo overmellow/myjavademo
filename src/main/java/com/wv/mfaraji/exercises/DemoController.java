a5a3671c6fe81dcaf603481e4e00ab58f9c62700
overmellow <morteza_faraji@yahoo.com>
2021-01-13 01:25:06 -0800

    add name

--- src/main/java/com/wv/mfaraji/exercises/DemoController.java
+++ src/main/java/com/wv/mfaraji/exercises/DemoController.java
@@ -1,13 +1,14 @@
 package com.wv.mfaraji.exercises;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
 	@RequestMapping("/hello")
-	public String sayHi() {
-		return "Hi";
+	public String sayHi(@RequestParam String name) {
+		return "Hi " + name + "!";
 	}
 }
